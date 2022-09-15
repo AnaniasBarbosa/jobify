6351b2043580922a6f5ad19c2df761eefa879b91
Ananias Barbosa <ananiasgomes27@gmail.com>
2022-09-14 21:56:11 -0300

    chore: atualizado projeto

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const express = require("express");
+const app = express();
+
 const connection = require("./database/database");
 const Categorias = require("./database/categorias");
 const Vagas = require("./database/vagas");
@@ -11,18 +14,103 @@ connection
     console.log("ERRO AO CONECTAR NO BANCO DE DADOS: ", error);
   });
 
-async function consultaBD() {
-  const vagas = await Vagas.findAll({ raw: true });
-  const categorias = await Categorias.findAll({ raw: true });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-  const conjunto = categorias.map((cat) => {
+app.get("/", async (req, res) => {
+  const vagasBd = await Vagas.findAll({ raw: true });
+  const categoriasBd = await Categorias.findAll({ raw: true });
+
+  const categorias = categoriasBd.map((cat) => {
     return {
-      cat,
-      vagas: vagas.filter((vg) => vg.categoria === cat.id),
+      ...cat,
+      vagas: vagasBd.filter((vg) => vg.categoria === cat.id),
     };
   });
+  console.log(categorias);
+  res.render("home", {
+    categorias,
+  });
+});
 
-  console.log(conjunto[1].vagas);
-}
+app.get("/vaga/:id", async (req, res) => {
+  const idVaga = req.params.id;
 
-consultaBD();
+  const vaga = await Vagas.findByPk(idVaga, { raw: true });
+  const categoria = await Categorias.findByPk(vaga.categoria, { raw: true });
+
+  return res.render("vaga", {
+    vaga,
+    categoria,
+  });
+});
+
+app.get("/admin", async (req, res) => {
+  res.render("admin/home.ejs");
+});
+
+app.get("/admin/vagas", async (req, res) => {
+  const vagas = await Vagas.findAll({ raw: true });
+  res.render("admin/vagas", {
+    vagas,
+  });
+});
+app.get("/admin/categorias", async (req, res) => {
+  res.render("admin/categorias");
+});
+
+app.get("/admin/vagas/remover/:id", async (req, res) => {
+  const vagaId = req.params.id;
+  const vaga = await Vagas.findByPk(vagaId);
+  await vaga.destroy();
+  res.redirect("/admin/vagas");
+});
+
+app.get("/admin/vagas/criarVaga/", async (req, res) => {
+  const categorias = await Categorias.findAll({ raw: true });
+  res.render("admin/nova-vaga", {
+    categorias,
+  });
+});
+
+app.post("/admin/vagas/criarVaga/", async (req, res) => {
+  const { categoria, nome, descricao } = req.body;
+
+  await Vagas.create({
+    categoria,
+    nome,
+    descricao,
+  });
+  return res.redirect("/admin/vagas/");
+});
+
+app.get("/admin/vagas/editar/:id", async (req, res) => {
+  const idVaga = req.params.id;
+  const vaga = await Vagas.findByPk(idVaga);
+  const categorias = await Categorias.findAll({ raw: true });
+  res.render("admin/editar-vaga", {
+    categorias,
+    vaga,
+  });
+});
+
+app.post("/admin/vagas/editar/:id", async (req, res) => {
+  const { categoria, nome, descricao } = req.body;
+  const { id } = req.params;
+
+  const vaga = await Vagas.findByPk(id);
+  vaga.descricao = descricao;
+  vaga.nome = nome;
+  vaga.categoria = categoria;
+
+  await vaga.save();
+  res.redirect("/admin/vagas/");
+});
+
+app.listen(3000, (err) => {
+  if (!err) {
+    console.log("SERVIDOR EM EXECUÇÃO");
+  }
+});
